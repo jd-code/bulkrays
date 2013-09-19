3f29636e075eabd707e238ff778d860a19f7a747
Jean-Daniel Pauget <jd@nkdn.fr>
2013-09-19 15:37:17 +0200

    first cast of hcpp

--- Makefile
+++ Makefile
@@ -1,33 +1,37 @@
 
 #DEBUG=
 DEBUG=-g
-CPPFLAGS=-Wnon-virtual-dtor
+CPPFLAGS=-Wnon-virtual-dtor 
+#CPPFLAGS=-Wnon-virtual-dtor -no-integrated-cpp -wrapper ./wrapper.sh
 INCLUDES=-I./include -I./qiconn/include
 PREFIX=/usr/local
 SHELL=/bin/sh
-VERSION=0.0.5
+VERSION=0.0.6
 
 default: all
 
 all: bulkrays
 
 allstrip: all
-	strip bulkrays
-
+	strip bulkrays hcpp2cpp
 
 vimtest: all
 	# ddd -args ./bulkrays --bind=127.0.0.1:10080 --user=$$USER --access_log=access_log --earlylog --console --p tagazou zonzon=2 p=3 BulkRays::ownsetoftests &
+	# ./hcpp2cpp testbb.hcpp -o testbb.cpp
 	./bulkrays --bind=127.0.0.1:10080 --user=$$USER		\
 	    --access_log=access_log --earlylog --console	\
 	    --p tagazou zonzon=2 p=3				\
 	    BulkRays::ownsetoftests				\
 	    2>&1 | unbuffer -p tr ':' '='
 
+hcpp2cpp: hcpp2cpp.cpp
+	g++ -Wall -o hcpp2cpp hcpp2cpp.cpp
+
 bulkrays: bulkrays.o qiconn/qiconn.o testsite.o bootstrap.o testsite.o simplefmap.o include/bulkrays/bulkrays.h
 	g++ ${DEBUG} ${CPPFLAGS} ${INCLUDES} -Wall -o  bulkrays bulkrays.o qiconn/qiconn.o bootstrap.o testsite.o simplefmap.o
 
-bulkrays.o: bulkrays.cpp include/bulkrays/bulkrays.h qiconn/include/qiconn/qiconn.h
-	g++ ${DEBUG} ${CPPFLAGS} ${INCLUDES} -DBULKRAYSVERSION="\"${VERSION}\"" -Wall -c bulkrays.cpp
+bulkrays.o: bulkrays.cc include/bulkrays/bulkrays.h qiconn/include/qiconn/qiconn.h
+	g++ ${DEBUG} ${CPPFLAGS} ${INCLUDES} -DBULKRAYSVERSION="\"${VERSION}\"" -Wall -c bulkrays.cc
 
 
 qiconn/qiconn.o: qiconn/qiconn.cpp qiconn/include/qiconn/qiconn.h
@@ -37,6 +41,7 @@ qiconn/qiconn.o: qiconn/qiconn.cpp qiconn/include/qiconn/qiconn.h
 include/bulkrays/bulkrays.h: qiconn/include/qiconn/qiconn.h
 
 clean:
+	rm -f *.cc
 	rm -f *.o bulkrays
 	rm -rf bulkrays-doc
 	( cd qiconn ; make clean )
@@ -59,4 +64,12 @@ testsite.o: testsite.cpp include/bulkrays/bulkrays.h
 
 simplefmap.o: simplefmap.cpp include/bulkrays/bulkrays.h
 	g++ ${DEBUG} ${CPPFLAGS} ${INCLUDES} -Wall -c simplefmap.cpp
+
+# bulkrays.cpp: bulkrays.hcpp hcpp2cpp
+
+#	./hcpp2cpp bulkrays.hcpp -o bulkrays.cpp
+
+
+%.cc: %.hcpp
+	./hcpp2cpp $< -o $@
 
---
+++ hcpp2cpp.cpp
@@ -0,0 +1,135 @@
+
+#include <errno.h>
+#include <string.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool atbeginingosstring = true;
+
+void escape (ostream & out, char c) {
+    switch (c) {
+	case 10:
+	case 13:
+		    if (!atbeginingosstring)
+			out << "\\015\\012\"";
+		    out << c;
+		    atbeginingosstring = true;
+		    break;
+
+	default:
+	    if (atbeginingosstring) {
+		out << '"';
+		atbeginingosstring = false;
+	    }
+	    switch (c) {
+		case '"':   out << "\\\"";  break;
+		case '\\':  out << "\\\\";  break;
+		default:    out << c;	    break;
+	    }
+    }
+}
+
+bool parse (istream &in, ostream &out) {
+    string  matchbegin ("{{"),
+	    matchend ("}}");
+#define     MATCHLEN 2
+    string const * match = &matchbegin;
+    bool incpp = true;
+    char c;
+    size_t pos = 0;
+
+    while (in && in.get(c)) {
+	if (c == (*match)[pos]) {
+	    pos ++;
+	    if (pos == MATCHLEN) {
+		if (incpp) {
+		    incpp = false;
+		    match = &matchend;
+		    atbeginingosstring = true;
+		    out << " ";
+		} else {
+		    incpp = true;
+		    match = &matchbegin;
+		    if (!atbeginingosstring)
+			out << '"';
+		    out << " ";
+		}
+		pos = 0;
+	    }
+	} else {
+	    if (pos != 0) {
+		for (size_t i=0 ; i<pos ; i++) {
+		    if (incpp)
+			out << (*match)[i];
+		    else
+			escape(out, (*match)[i]);
+		}
+		pos = 0;
+	    }
+	    if (incpp)
+		out << c;
+	    else
+		escape(out, c);
+	}
+    }
+    return true;
+}
+
+int main (int nb, char ** cmde) {
+
+    string outputfname;
+
+    int i;
+    for (i=1 ; i<nb ; i++) {	// let's seek for the output name
+	if (strncmp (cmde[i], "-o", 2) == 0) {
+	    if (cmde[i][2] != 0) {
+		outputfname += (cmde[i] + 2);
+	    } else if (i+1 < nb) {
+		outputfname += cmde[i+1];
+	    }
+	    break;
+	}
+    }
+
+    if (outputfname.empty()) {
+	cerr << "hcpp2cpp  error, missing filename output" << endl;
+	return 1;
+    }
+
+    ofstream out(outputfname.c_str());
+
+    if (!out) {
+	int e = errno;
+	cerr << "hcpp2cpp  error, cannot open " << outputfname << " , "
+	     << strerror(e) << endl;
+	return 1;
+    }
+
+    for (i=1 ; i<nb ; i++) {
+	if (strncmp (cmde[i], "-o", 2) == 0) {
+	    if (cmde[i][2] == 0) {
+		i++;
+	    }
+	    continue;
+	}
+
+	ifstream in(cmde[i]);
+	if (!in) {
+	    int e = errno;
+	    cerr << "hcpp2cpp  error, cannot open " << cmde[i] << " , "
+		 << strerror(e) << endl;
+	    return 1;
+	}
+
+cerr << "parsing " << cmde[i] << " ..." << endl;
+	out << "# 1 \"" << cmde[i] << "\"" << endl;
+	if (!parse (in, out))
+	    break;
+    }
+
+    return 0;
+}
